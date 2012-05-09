0c8c9cf01a5a9056071919a7287cfc11099c13f5
Samuel Portela <negociosnainternet@gmail.com>
2012-05-09 01:45:27 -0300

    faye channels created to update users auctions and specific user remaining bids (remaining bids being updated in multiple devices when the user is logged in more than one browser)

--- server.js
+++ server.js
@@ -1,73 +1,41 @@
 var express = require('express'),
-  nowjs = require("now"),
-  countdown = require('./countdown.js'),
-  $ = require('jquery'),
-  allowedRequesters = [],
+  Faye = require('faye'),
   port,
-  app,
-  everyone,
-  timer,
-  updateCounter;
+  allowedRequesters,
+  expressServer,
+  fayeServer;
 
-timer = new countdown.CountdownTimer({
-  seconds: 30,
-  onTick: function(hour, minute, second) {
-    updateCounter(hour, minute, second);
-  },
-  onComplete: function() {
-    console.log('complete');
-  }
-});
-
-updateCounter = function(hour, minute, second) {
-  console.log('Antes: ' + hour + ':' + minute + ':' + second);
-  if (everyone.now.receberMensagem) {
-    everyone.now.receberMensagem('This is an update', hour + ':' + minute + ':' + second);
-  }
-  console.log('Depois: ' + hour + ':' + minute + ':' + second);
-}
-
-timer.start();
-
-// configuring application
-allowedRequesters.push('127.0.0.1');
 port = 8000;
+allowedRequesters = ['127.0.0.1'];
 
-// initializing expressjs
-app = express.createServer();
-app.listen(port);
+expressServer = express.createServer();
+expressServer.use(express.bodyParser());
 
-// initializing nowjs
-everyone = nowjs.initialize(app);
+fayeServer = new Faye.NodeAdapter({mount: '/faye'});
 
-// nowjs code
-everyone.now.sendMessage = function (message) {
-    everyone.now.receberMensagem(this.now.name, message);
-};
-
-// expressjs code
-app.use(express.bodyParser());
-
-app.all('*', function (request, response, next) {
-    if (allowedRequesters.indexOf(request.connection.remoteAddress) != -1) {
-        next();
-    } else {
-        // console.log('This address does not have access: ' + request.connection.remoteAddress + '\n');
-        response.writeHead(404);
-        response.write('Your address: ' + request.connection.remoteAddress + '\n');
-        response.end();
-    }
-});
-
-app.get('/', function (request, response) {
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    response.write('Your address: ' + request.connection.remoteAddress + '\n');
+expressServer.all('*', function (request, response, next) {
+  if (allowedRequesters.indexOf(request.connection.remoteAddress) == -1) {
+    response.writeHead(403);
     response.end();
+  } else {
+    next();
+  }
 });
 
-app.post('/message', function (request, response) {
-    everyone.now.receberMensagem('Mensagem do sistema', request.body.message);
-    response.end();
+expressServer.post('/notifyBidCreation', function(req, res) {
+  fayeServer.getClient().publish('/auctionsChannel', {
+    userDisplayName: req.body.userDisplayName,
+    auctionId: req.body.auctionId,
+    auctionEndPrice: req.body.auctionEndPrice,
+    auctionRemainingTime: req.body.auctionRemainingTime
+  });
+  
+  fayeServer.getClient().publish('/remainingBidsChannel/' + req.body.userId, {
+    userBidsToDecrease: req.body.userBidsToDecrease
+  });
+  
+  res.send(req.body);
 });
+
+fayeServer.attach(expressServer);
+expressServer.listen(port);
